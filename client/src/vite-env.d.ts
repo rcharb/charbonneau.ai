2c62bef41e0aff9c38a65eb9e56909f2bb3539f8
techdev-loop <vadymyashen@gmail.com>
2025-12-19 05:55:58 +0900

    fix: add frontend env variable to dockerfiles

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_ENABLE_LOGGER: string;
   readonly VITE_LOGGER_FILTER: string;
   // Add other env variables here
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
+  readonly VITE_STRIPE_PRICING_TABLE_ID_LIGHT: string;
+  readonly VITE_STRIPE_PRICING_TABLE_ID_DARK: string;
 }
 
 interface ImportMeta {
